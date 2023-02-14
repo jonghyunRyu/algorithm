867e023f7fbe0018d23eeb0cbb1d799a2f8fc14e
willowise <willowise@naver.com>
2023-02-14 17:40:16 +0900

    백준 입출력과 사칙연산 자바

---
+++ baekjoon/src/입출력과 사칙연산/Sprout.java
@@ -0,0 +1,16 @@
+//백준 입출력과 사칙연산 25083번 문제 
+// ', ", `, \ 등의 문자에 주의하며 새싹을 출력하는 문제
+
+public class Sprout {
+    public static void main(String[] args) {
+        // \ 이스케이프 문자를 이용해 ', ", \ 등을 출력함.
+        String str ="         ,r'\"7\n" +
+                    "r`-_   ,\'  ,/ \n" +
+                    " \\. \". L_r\'\n" +
+                    "   `~\\/\n" +
+                    "      |\n" + 
+                    "      |";
+        
+    System.out.println(str);
+    }
+}
