2729dd2d3af32aff9c5c040587662617748bf0aa
willowise <willowise@naver.com>
2023-02-27 18:55:51 +0900

    no message

--- baekjoon/src/입출력과 사칙연산/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-//백준 입출력과 사칙연산 2557번 문제
-//Hello World!를 화면에 출력하는 문제
-public class HelloWorld {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
-    }
-}
