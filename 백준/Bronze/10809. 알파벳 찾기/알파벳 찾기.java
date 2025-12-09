78467d95a504a5ec42a521e29881d6be48780bc1
종현종현 <80243566+jonghyunRyu@users.noreply.github.com>
2025-12-10 08:48:06 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		int[] alpha = new int[26];
+		
+		for(int i=0; i < alpha.length; i++) {
+			alpha[i]= -1; 
+		}
+		
+		for(int i=0; i < s.length(); i++) {
+			if (alpha[s.charAt(i)-97] == -1) {
+				alpha[s.charAt(i)-97] = i; 
+			} 
+		}
+		for(int i=0; i < alpha.length; i++) {
+			System.out.print(alpha[i]+" ");
+		}
+	}
+}
