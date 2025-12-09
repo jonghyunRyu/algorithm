c4cb43dbe4f529c2fe0ff90ec771fc5aa5302e76
종현종현 <80243566+jonghyunRyu@users.noreply.github.com>
2025-12-10 08:48:16 +0900

    [Bronze III] Title: 최소, 최대, Time: 436 ms, Memory: 120312 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int min = Integer.parseInt(st.nextToken());
+        int max = min;
+        
+        for (int i = 1; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (arr[i] < min) {
+                min = arr[i];
+            } else if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(min + " " + max);
+    }
+}
