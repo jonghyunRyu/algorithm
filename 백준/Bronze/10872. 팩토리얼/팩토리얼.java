6721168464cf0ec85d1d55e3d4b51265f9842e58
종현종현 <80243566+jonghyunRyu@users.noreply.github.com>
2025-12-10 08:48:22 +0900

    [Bronze III] Title: 팩토리얼, Time: 80 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(factorial(N));
+    }
+    static int factorial(int n) {
+        if (n > 0) {
+            return n * factorial(n-1);
+        } else {
+            return 1;
+        }
+    }
+}
