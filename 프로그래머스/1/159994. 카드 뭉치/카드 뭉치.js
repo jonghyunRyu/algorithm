31ea88f570860b6c46664dea6cd479572772c311
류종현 <80243566+jonghyunRyu@users.noreply.github.com>
2026-04-05 22:57:33 +0900

    [level 1] Title: 카드 뭉치, Time: 0.16 ms, Memory: 32 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,15 @@
+function solution(cards1, cards2, goal) {
+    for (let i = 0; i < goal.length; i++) {
+        if (goal[i] === cards1[0]) {
+            cards1.shift();
+            continue;
+        } else if (goal[i] === cards2[0]) {
+            cards2.shift();
+            continue;
+        } else if (goal[i] !== cards1[0] && goal[i] !== cards2[0]) {
+            return "No";
+        }
+        
+    }
+    return "Yes";
+}
