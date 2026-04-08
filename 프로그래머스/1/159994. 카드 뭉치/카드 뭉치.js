f7f3ebcda231a67d6886c762f962be5fc8d92e88
류종현 <80243566+jonghyunRyu@users.noreply.github.com>
2026-04-08 22:14:45 +0900

    [level 1] Title: 카드 뭉치, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,15 +1,31 @@
+// 처음 풀이 shift() 메서드 사용 -> 배열의 길이가 길어지면 효율이 떨어짐 시간복잡도 증가
+// function solution(cards1, cards2, goal) { 
+//     for (let i of goal) {
+//         if (i === cards1[0]) {
+//             cards1.shift();
+//         } else if (i === cards2[0]) {
+//             cards2.shift();
+//         } else {
+//             return "No";
+//         }
+//     }
+    
+//     return "Yes";
+// }
+
 function solution(cards1, cards2, goal) {
+    let j = 0;
+    let k = 0;
+    
     for (let i = 0; i < goal.length; i++) {
-        if (goal[i] === cards1[0]) {
-            cards1.shift();
-            continue;
-        } else if (goal[i] === cards2[0]) {
-            cards2.shift();
-            continue;
-        } else if (goal[i] !== cards1[0] && goal[i] !== cards2[0]) {
+        if (goal[i] === cards1[j]) {
+            j++;
+        } else if (goal[i] === cards2[k]) {
+            k++;
+        } else {
             return "No";
         }
-        
     }
+    
     return "Yes";
 }
