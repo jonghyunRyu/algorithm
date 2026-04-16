5ed6ddd326efc8c39a266f371faab57fcf35a48c
류종현 <80243566+jonghyunRyu@users.noreply.github.com>
2026-04-16 20:12:53 +0900

    [level 2] Title: 괄호 회전하기, Time: 34.79 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,38 +1,67 @@
+// function solution(s) {
+//     let answer = 0;
+    
+//     let stack = [];
+//     let strArr = s.split('');
+    
+//     for (let i = 0; i < strArr.length; i++) {
+//         for (let j = 0; j < strArr.length; j++) {
+//             if (stack.length === 0) {
+//                 stack.push(strArr[j]);
+//             } else {
+//                 if (stack[0] === ')' || stack[0] === ']' || stack[0] === '}') break;
+//                 if (stack[stack.length - 1] === '(' && strArr[j] !== ')') {
+//                     stack.push(strArr[j]);
+//                 } else if (stack[stack.length - 1] === '(' && strArr[j] === ')') {
+//                     stack.pop();
+//                 } else if (stack[stack.length - 1] === '[' && strArr[j] !== ']') {
+//                     stack.push(strArr[j]);
+//                 } else if (stack[stack.length - 1] === '[' && strArr[j] === ']') {
+//                     stack.pop();
+//                 } else if (stack[stack.length - 1] === '{' && strArr[j] !== '}') {
+//                     stack.push(strArr[j]);
+//                 } else if (stack[stack.length - 1] === '{' && strArr[j] === '}') {
+//                     stack.pop();
+//                 } else {
+//                     stack.push(strArr[j]);
+//                 }
+//             }
+//         }
+        
+//         if (stack.length === 0) answer++;
+//         stack = [];
+//         const c = strArr.shift();
+//         strArr = [...strArr, c];
+//     }
+    
+//     return answer;
+// }
+
 function solution(s) {
+    if(s.length % 2 === 1) return 0;
+
     let answer = 0;
-    
-    let stack = [];
-    let strArr = s.split('');
-    
-    for (let i = 0; i < strArr.length; i++) {
-        for (let j = 0; j < strArr.length; j++) {
-            if (stack.length === 0) {
-                stack.push(strArr[j]);
-            } else {
-                if (stack[0] === ')' || stack[0] === ']' || stack[0] === '}') break;
-                if (stack[stack.length - 1] === '(' && strArr[j] !== ')') {
-                    stack.push(strArr[j]);
-                } else if (stack[stack.length - 1] === '(' && strArr[j] === ')') {
-                    stack.pop();
-                } else if (stack[stack.length - 1] === '[' && strArr[j] !== ']') {
-                    stack.push(strArr[j]);
-                } else if (stack[stack.length - 1] === '[' && strArr[j] === ']') {
-                    stack.pop();
-                } else if (stack[stack.length - 1] === '{' && strArr[j] !== '}') {
-                    stack.push(strArr[j]);
-                } else if (stack[stack.length - 1] === '{' && strArr[j] === '}') {
-                    stack.pop();
-                } else {
-                    stack.push(strArr[j]);
+    const mapping = { "}" : "{", "]" : "[", ")" : "("};
+
+    for(let i = 0; i < s.length; i++) {
+        const stack = [];
+        const rotate = s.slice(i) + s.slice(0, i);
+        let flag = true;
+
+        for(let j = 0; j < s.length; j++) {
+            if(rotate[j] === "[" || rotate[j] === "(" || rotate[j] === "{" )
+                stack.push(rotate[j]);
+            else {
+                const last = stack.pop();
+                if(last !== mapping[rotate[j]]) {
+                    flag = false
+                    break;
                 }
             }
         }
-        
-        if (stack.length === 0) answer++;
-        stack = [];
-        const c = strArr.shift();
-        strArr = [...strArr, c];
+
+        if(flag) answer++;
     }
-    
+
     return answer;
 }
