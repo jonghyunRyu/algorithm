97cb2882364bfcea998f5c640b1adaad579caf0c
류종현 <80243566+jonghyunRyu@users.noreply.github.com>
2026-05-07 17:37:19 +0900

    [level 2] Title: 주식가격, Time: 3.93 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,18 +1,42 @@
+// function solution(prices) {
+    
+//     let answer = [];
+    
+//     for (let i = 0; i < prices.length; i++) {
+        
+//         const price = prices[i];
+//         let period = 0;
+        
+//         for (let j = i + 1; j < prices.length; j++) {
+//             period++;
+//             if (prices[j] < price) break;
+//         }
+        
+//         answer.push(period);
+//     }
+    
+//     return answer;
+// }
+
+// 시간복잡도 개선
 function solution(prices) {
     
-    let answer = [];
+    const n = prices.length;
+    const answer = new Array(n).fill(0);
     
-    for (let i = 0; i < prices.length; i++) {
-        
-        const price = prices[i];
-        let period = 0;
-        
-        for (let j = i + 1; j < prices.length; j++) {
-            period++;
-            if (prices[j] < price) break;
+    const stack = [0];
+    for (let i = 1; i < n; i++) {
+        while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
+            const j = stack.pop();
+            answer[j] = i - j;
         }
         
-        answer.push(period);
+        stack.push(i);
+    }
+    
+    while (stack.length > 0) {
+        const j = stack.pop();
+        answer[j] = n - 1 - j;
     }
     
     return answer;
