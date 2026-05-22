f54c03caee61976c5797a276defc99e47f9372f8
류종현 <80243566+jonghyunRyu@users.noreply.github.com>
2026-05-22 15:34:40 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.09 ms, Memory: 32.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,6 @@
+function solution(num_list) {
+    const odd = num_list.filter(n => n % 2 !== 0).join('');
+    const even = num_list.filter(n => n % 2 === 0).join('');
+    
+    return Number(odd) + Number(even);
+}
