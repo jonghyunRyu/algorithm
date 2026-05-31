77d2db9c87e0c88ce97e40607194f9d2e865531e
류종현 <80243566+jonghyunRyu@users.noreply.github.com>
2026-05-31 09:07:53 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 32.6 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,8 +1,7 @@
 function solution(box, n) {
     
-    const a = Math.floor(box[0] / n);
-    const b = Math.floor(box[1] / n);
-    const c = Math.floor(box[2] / n);
+    const [a, b, c] = box;
     
-    return a * b * c;
+    
+    return Math.floor(a / n) * Math.floor(b / n) * Math.floor(c / n);
 }
