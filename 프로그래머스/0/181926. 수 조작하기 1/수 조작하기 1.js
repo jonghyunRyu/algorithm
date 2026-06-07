312de2ff531d2c105685b3d9eefea50f66ca3b9f
류종현 <80243566+jonghyunRyu@users.noreply.github.com>
2026-06-07 09:35:11 +0900

    [level 0] Title: 수 조작하기 1, Time: 3.73 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,10 @@
+function solution(n, control) {
+    const con = {
+        'w' : 1,
+        's' : -1,
+        'd' : 10,
+        'a' : -10
+    };
+    
+    return n + [...control].reduce((a, v) => a + con[v], 0);
+}
