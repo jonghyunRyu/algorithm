7fd83d4b0202f7766d4e40c88d8765aabfbdc902
류종현 <80243566+jonghyunRyu@users.noreply.github.com>
2026-08-27 15:28:01 +0900

    [level 0] Title: 문자열 섞기, Time: 0.10 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -0,0 +1,11 @@
+function solution(str1, str2) {
+    
+    let answer = '';
+    
+    for (let i = 0; i < str1.length * 2; i++) {
+        if (i % 2 === 0) answer += str1[i / 2];
+        else answer += str2[Math.floor(i / 2)];
+    } 
+    
+    return answer;
+}
